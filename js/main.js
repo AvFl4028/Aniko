a9f64a8e75a4e986442de336ed9b484e2470b6c0
AvFl4028 <aviel4028@gmail.com>
2022-06-23 20:04:57 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const history = document.getElementById("btn-history");
+
+history.addEventListener("click", () => {
+    alert("Sigue pendiente XD");
+    //window.location.href = "history.html";
+})
